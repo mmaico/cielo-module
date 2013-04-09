7284a1c66999d9ba3ea95a73174e9788d7554738
mmaco <mmaico@gmail.com>
2013-04-09 00:18:05 -0300

    Creating method to find transactions and adjustments unit tests.

--- src/main/java/br/com/kohen/module/cielo/ws/impl/CieloWebServiceImpl.java
+++ src/main/java/br/com/kohen/module/cielo/ws/impl/CieloWebServiceImpl.java
@@ -14,6 +14,7 @@ import br.com.kohen.module.cielo.entity.CieloResponse;
 import br.com.kohen.module.cielo.entity.CieloTransaction;
 import br.com.kohen.module.cielo.exception.ConnectionFailedException;
 import br.com.kohen.module.cielo.utils.PropertiesAcessor;
+import br.com.kohen.module.cielo.utils.XmlTemplateReader.TemplateTransaction;
 import br.com.kohen.module.cielo.ws.CieloWebService;
 
 public class CieloWebServiceImpl implements CieloWebService {
@@ -25,11 +26,27 @@ public class CieloWebServiceImpl implements CieloWebService {
 	private static final String URL_WS = PropertiesAcessor.load().getProperty("cielo.url.webservice");
 	
 	public CieloResponse newTransaction(CieloTransaction transaction) {
+		
+		return callWS(transaction, NEW);
+	}
+
+	public CieloResponse findTransaction(String tid, BusinessEstablishment bEstablishment) {
+		
+		CieloTransaction transaction = CieloTransaction.build()
+			.withBusinessEstablishment(bEstablishment)
+			.withTid(tid);
+		
+		return callWS(transaction, CHECK);
+	}
+	
+	private CieloResponse callWS(CieloTransaction transaction, TemplateTransaction template) {
+		
 		String content = "";
 		try {
+			
 			content = Request.Post(URL_WS)
 					.connectTimeout(_CONNECTION_TIMEOUT)
-					.bodyForm(Form.form().add("mensagem", transaction.toXml(NEW)).build())
+					.bodyForm(Form.form().add("mensagem", transaction.toXml(template)).build())
 					.execute().returnContent().asString();
 			
 		} catch (ClientProtocolException e) {
@@ -39,27 +56,6 @@ public class CieloWebServiceImpl implements CieloWebService {
 		}
 		
 		return CieloResponse.build(content);
-	}
-
-	public CieloResponse findTransaction(String tid, BusinessEstablishment bEstablishment) {
-//		String content = "";
-//		try {
-//			CieloTransaction transaction = CieloTransaction.build()
-//				.withBusinessEstablishment(bEstablishment)
-//				.withTid(tid);
-//			
-//			content = Request.Post(URL_WS)
-//					.connectTimeout(_CONNECTION_TIMEOUT)
-//					.bodyForm(Form.form().add("mensagem", transaction.toXml(CHECK)).build())
-//					.execute().returnContent().asString();
-//			
-//		} catch (ClientProtocolException e) {
-//			throw new ConnectionFailedException(e);
-//		} catch (IOException e) {
-//			throw new ConnectionFailedException(e);
-//		}
-//		
-		return null;//CieloResponse.build(content);
 	}
 
 }
--- src/test/java/br/com/kohen/module/cielo/ws/impl/CieloWebServiceImplTest.java
+++ src/test/java/br/com/kohen/module/cielo/ws/impl/CieloWebServiceImplTest.java
@@ -3,25 +3,35 @@ package br.com.kohen.module.cielo.ws.impl;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.xml.bind.DatatypeConverter;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
+import org.custommonkey.xmlunit.Diff;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.xml.sax.SAXException;
 
 import br.com.kohen.module.cielo.entity.BusinessEstablishment;
 import br.com.kohen.module.cielo.entity.CieloOrder;
@@ -34,8 +44,9 @@ import br.com.kohen.module.cielo.enums.Currency;
 import br.com.kohen.module.cielo.enums.Language;
 import br.com.kohen.module.cielo.enums.Modality;
 
+@SuppressWarnings({ "unchecked"})
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Request.class})
+@PrepareForTest({Request.class, Form.class})
 public class CieloWebServiceImplTest {
 
 	private static final String URL = "https://qasecommerce.cielo.com.br/servicos/ecommwsec.do";
@@ -48,9 +59,12 @@ public class CieloWebServiceImplTest {
 	
 	@Mock Content contectMock;
 	
+	@Captor ArgumentCaptor<List<NameValuePair>> listNameValueCaptor;
+	
 	@Before
 	public void setUp() throws ClientProtocolException, IOException {
 		
+		mockStatic(Request.class);
 		given(Request.Post(URL)).willReturn(requestMock);
 		given(this.requestMock.connectTimeout(10000)).willReturn(requestMock);
 		
@@ -64,10 +78,11 @@ public class CieloWebServiceImplTest {
 		
 		String urlAuthentication = "https://ecommerce.cielo.com.br/web/index.cbmp?id=a783251";
 		String tid = "10017348980735271001";
-		String templateToSend = ReadXmlToTest.read("/xmlExpected/requisicao-transacao.buypagecielo-template.xml");
+		String id = "6-e7762cbf8856";
+		
 		String returnWillBe = ReadXmlToTest.read("/xmlReturn/newTrasaction-success-buy-cielo-page-return.xml");
 		
-		given(this.requestMock.bodyForm(Form.form().add("mensagem", templateToSend).build())).willReturn(requestMock);
+		given(this.requestMock.bodyForm(Mockito.anyCollection())).willReturn(requestMock);
 		given(this.contectMock.asString()).willReturn(returnWillBe);
 
 		CieloResponse newTransaction = service.newTransaction(getTransactionStub());
@@ -76,6 +91,68 @@ public class CieloWebServiceImplTest {
 		
 		assertThat(transaction.getUrlAuthentication(), equalTo(urlAuthentication));
 		assertThat(transaction.getTid(), equalTo(tid));
+		assertThat(transaction.getId(), equalTo(id));
+	}
+	
+	@Test
+	public void shouldVerifyXmlToSend() throws ParseException, SAXException, IOException {
+		
+		String templateToSendExpected = ReadXmlToTest.read("/xmlExpected/requisicao-transacao.buypagecielo-template.xml");
+		String returnWillBe = ReadXmlToTest.read("/xmlReturn/newTrasaction-success-buy-cielo-page-return.xml");
+		
+		given(this.requestMock.bodyForm(Mockito.anyCollection())).willReturn(requestMock);
+		given(this.contectMock.asString()).willReturn(returnWillBe);
+		
+		service.newTransaction(getTransactionStub());
+		
+		verify(this.requestMock).bodyForm(listNameValueCaptor.capture());
+		
+		List<NameValuePair> listCaptured = listNameValueCaptor.getValue();
+		Diff diff = new Diff(templateToSendExpected, listCaptured.get(0).getValue());
+		
+		assertThat(listCaptured.get(0).getName(), equalTo("mensagem"));
+		assertThat(diff.similar(), Matchers.is(Boolean.TRUE));
+	}
+	
+	@Test
+	public void shouldFindATransactionByIdTid() throws IOException, ParseException {
+		
+		String tid = "10017348980735271001";
+		
+		String returnWillBe = ReadXmlToTest.read("/xmlReturn/newTrasaction-success-buy-cielo-page-return.xml");
+		
+		given(this.requestMock.bodyForm(Mockito.anyCollection())).willReturn(requestMock);
+		given(this.contectMock.asString()).willReturn(returnWillBe);
+
+		CieloResponse response = service.findTransaction(tid, getTransactionStub().getbEstablishment()) ;
+		
+		CieloTransaction transaction = response.getTransaction();
+		
+		assertThat(transaction.getTid(), equalTo(tid));
+	}
+	
+	@Test
+	public void shouldVerifyXmlToSendToConsult() throws ParseException, SAXException, IOException {
+		
+		String tid = "10017348980735271001";
+		
+		String xmlToSend = ReadXmlToTest.read("/xmlExpected/requisicao-consulta-template.xml");
+		String willReturn = ReadXmlToTest.read("/xmlReturn/consult-success-buy-cielo-page-return.xml");
+		
+		given(this.requestMock.bodyForm(Mockito.anyCollection())).willReturn(requestMock);
+		given(this.contectMock.asString()).willReturn(willReturn);
+
+		CieloResponse response = service.findTransaction(tid, getTransactionStub().getbEstablishment()) ;
+		
+		response.getTransaction();
+		
+		verify(this.requestMock).bodyForm(listNameValueCaptor.capture());
+		
+		List<NameValuePair> listCaptured = listNameValueCaptor.getValue();
+		Diff diff = new Diff(xmlToSend, listCaptured.get(0).getValue());
+		
+		assertThat(listCaptured.get(0).getName(), equalTo("mensagem"));
+		assertThat(diff.similar(), Matchers.is(Boolean.TRUE));
 	}
 	
 	private CieloTransaction getTransactionStub() throws ParseException {
